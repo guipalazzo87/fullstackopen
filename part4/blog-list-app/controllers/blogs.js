d2c2fadd569d2a220da507434de37116bb5a2275
Guilherme Palazzo G. Rodrigues <55751309+guipalazzo87@users.noreply.github.com>
2021-04-28 22:20:37 -0300

    used async await in delete and put routes

--- part4/blog-list-app/controllers/blogs.js
+++ part4/blog-list-app/controllers/blogs.js
@@ -21,4 +21,27 @@ blogsRouter.post('/', async (request, response) => {
   
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+//TODO: blogsRouter.delete is return 204 even when there wasn't any blog to delete
+
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0
+  }
+  
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new : true})
+  
+ 
+  response.json({updatedBlog}).status(200).end()
+})
+
 module.exports = blogsRouter
